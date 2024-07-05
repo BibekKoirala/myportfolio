923d17fcc355ac1c7644ce1131e4872abb8d45fc
Bibek Koirala <kbibek371@gmail.com>
2024-07-04 22:54:29 -0400

    Notgound route added

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 				{/* <Route path="/articles" element={<Articles />} /> */}
 				{/* <Route path="/article/:slug" element={<ReadArticle />} /> */}
 				<Route path="/contact" element={<Contact />} />
-				{/* <Route path="*" element={<Notfound />} /> */}
+				<Route path="*" element={<Notfound />} /> 
 			</Routes>
 			</div>
 		</div>
